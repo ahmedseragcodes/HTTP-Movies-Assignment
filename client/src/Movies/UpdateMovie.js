ac28b00849cda75143dc0558c8c98295dc1fcb90
ahmedseragcodes <ahmedserag1@gmail.com>
2021-02-24 17:47:18 -0500

    installed dependencies and setup file structure

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class UpdateMovie extends React.Component {
+    constructor(){
+        super();
+        this.state= {
+            movieTitle: test,
+            movieDirector: test, 
+            movieScore: test,
+
+        }
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+    
+    }
+}
